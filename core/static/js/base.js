aaea277079692f5a7c7bd9436a431bf50c9f01e3
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-07-09 16:03:26 -0400

    create home page

--- core/static/js/base.js
+++
@@ -1,1 +0,0 @@
-const teamName = document.getElementById("team");
---
+++ core/static/js/home.js
@@ -0,0 +1,40 @@
+const projectCards = document.getElementById("project-cards");
+const myPortfolio = [
+  {
+    name: "Music Player",
+    description: "FreeCodeCamp JavaScript Course",
+    languages: "HTML, CSS, JavaScript",
+    link: "musicPlayer",
+    number: 7
+  },
+  {
+    name: "Football Cards",
+    description: "FreeCodeCamp JavaScript Course",
+    languages: "HTML, CSS, JavaScript",
+    link: "footballCards",
+    number: 10
+  }
+]
+
+
+Object.freeze(myPortfolio);
+
+
+const renderCards = (arr = myPortfolio) => {
+  console.log("setPlayerCards");
+  projectCards.innerHTML += arr
+    .map(
+      ({ name,description, languages }) =>
+        `
+        <div class="project-card">
+          <h2>${name}</h2>
+          <p>${description}</p>
+          <p>Languages: ${languages}</p>
+          <p><a href="#">View</a></p>
+        </div>
+      `
+    )
+    .join("");
+};
+
+renderCards();
