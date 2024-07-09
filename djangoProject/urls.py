0745268f2ba925c8a9bbb46e1c12dfa829d18a05
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-07-09 14:58:44 -0400

    initial commit

---
+++ djangoProject/urls.py
@@ -0,0 +1,15 @@
+# --- djangoProject/urls.py ---
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    # Django admin
+    path("admin/", admin.site.urls),
+
+    # User Management
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    # Local Apps
+    path("", include("core.urls")),
+]
